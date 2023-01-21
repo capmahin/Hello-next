885d41e72b18f50cdeb34f02f8260886eaf28e84
capmahin <mahinulhuque1996@gmail.com>
2023-01-22 01:03:54 +0600

    add elemnts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,16 @@
 import Document, {Html, Head, Main, NextScript} from "next/document";
 
 export default class CustomDocument extends Document {
-     
+    render(){
+        return <Html>
+            <Head>
+                <meta property="custom" content="yolo" />
+            </Head>
+            <body>
+            <Main/>
+            </body>
+            
+            <NextScript/>
+        </Html>
+    } 
 }
