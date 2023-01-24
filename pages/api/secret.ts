e36a960e8b733c168182bdd5f2b57ae1bb3fca57
capmahin <mahinulhuque1996@gmail.com>
2023-01-25 02:34:34 +0600

    add secretadminCode

---
+++ pages/api/secret.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from 'jsonwebtoken'
+
+const KEY = 'asdsadasdsadasdasdas';
+
+export default function (req: NextApiRequest, res: NextApiResponse) {
+
+    const { token } = req.body
+
+    const { admin } = jwt.verify(token, KEY) as {
+        [key: string]: boolean
+
+
+    }
+    if (admin) {
+        res.json({ secretAdminCode: 12345 })
+    } else {
+        res.json({ admin: false })
+    }
+
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,7 @@ export default function Home() {
   const [username, setUsername] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [message, setMessage] = useState<string>('You are not logged in')
+  const [secret, setSecret] = useState<string>('')
 
 
   async function submitFrom() {
@@ -38,6 +39,21 @@ export default function Home() {
 
       console.log(json)
       setMessage(`Welcome ${json.username} and you are ${json.admin ? 'an admin!' : 'not an admin!'}`)
+
+      const res = await fetch('/api/secret', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ token })
+      }).then((t) => t.json())
+
+      if (res.secretAdminCode) {
+        setSecret(res.secretAdminCode)
+      } else {
+        setSecret('Nothing available')
+      }
+
     } else {
       setMessage('Something went wrong')
     }
@@ -45,6 +61,7 @@ export default function Home() {
   }
   return <div>
     <h1>{message}</h1>
+    <h1>Secret:{secret}</h1>
     <form>
       <input type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} />
       <br />
