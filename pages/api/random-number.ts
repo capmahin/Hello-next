80f8b3ccd2dd29d5dff7f8789955fb06c0c6817e
capmahin <mahinulhuque1996@gmail.com>
2023-01-24 12:44:30 +0600

    add req.query

--- pages/api/random-number.ts
+++ pages/api/random-number.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
-    console.log('REQUEST BODY', req.body)
+    console.log('REQUEST BODY', req.query)
 
 
     res.json({ num: Math.floor(Math.random() * 10) })
