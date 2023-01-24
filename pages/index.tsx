169c88ff5b5b07b8d2d9d5b36b298b0a6999b1e9
capmahin <mahinulhuque1996@gmail.com>
2023-01-25 01:57:58 +0600

    change value to defaultValue

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,9 @@
 export default function Home() {
   return <div>
     <form method="POST" action="/api/login">
-      <input type="text" name="username" value="admin" />
+      <input type="text" name="username" defaultValue="admin" />
       <br />
-      <input type="password" name="password" value="admin" />
+      <input type="password" name="password" defaultValue="admin" />
       <br />
       <input type="submit" value="Login" />
     </form>
