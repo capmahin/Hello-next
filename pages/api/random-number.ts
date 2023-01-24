2a962246fe34e3b5412cfc1d825f22b6c7e5c772
capmahin <mahinulhuque1996@gmail.com>
2023-01-24 12:31:47 +0600

    console.log(req.body)

--- pages/api/random-number.ts
+++ pages/api/random-number.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
-    //it should NOT be a react component
+    console.log('REQUEST BODY', req.body)
 
 
     res.json({ num: Math.floor(Math.random() * 10) })
