a68755fc619e24512a06a58530ef3fffa74047a6
capmahin <mahinulhuque1996@gmail.com>
2023-01-25 02:16:57 +0600

    add change code in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 // import styles from './index.module.scss'
 
+import { useState } from "react"
+import jwt from 'jsonwebtoken'
+
 // //Style-JSX
 
 // function Heading(props) {
@@ -13,13 +16,36 @@
 // }
 
 export default function Home() {
+
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [message, setMessage] = useState<string>('You are not logged in')
+
+
+  async function submitFrom() {
+    const res = await fetch('/api/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password })
+    }).then((t) => t.json())
+
+    const token = res.token
+
+    if (token) {
+      const json = jwt.decode(token) as { [key: string]: string }
+      setMessage(`Welcome ${json.username} and you are ${json.admin ? 'an admin!' : 'not an admin!'}`)
+    } else {
+      setMessage('Something went wrong')
+    }
+
+  }
   return <div>
-    <form method="POST" action="/api/login">
-      <input type="text" name="username" defaultValue="admin" />
+    <h1>{message}</h1>
+    <form>
+      <input type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} />
       <br />
-      <input type="password" name="password" defaultValue="admin" />
+      <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
       <br />
-      <input type="submit" value="Login" />
+      <input type="button" value="Login" onClick={submitFrom} />
     </form>
 
   </div >
