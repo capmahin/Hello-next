de7cf21bbd886684026049145b9a09a1a7e24d42
capmahin <mahinulhuque1996@gmail.com>
2023-01-24 12:52:06 +0600

    add set-cookie

--- pages/api/random-number.ts
+++ pages/api/random-number.ts
@@ -2,8 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
-    console.log('REQUEST BODY', req.query)
 
+    res.setHeader('Set-Cookie', 'areyouprogrammer=true')
 
     res.json({ num: Math.floor(Math.random() * 10) })
 }
