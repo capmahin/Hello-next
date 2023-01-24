33d695638918d87cffd327ee284cca4dafe5a1a1
capmahin <mahinulhuque1996@gmail.com>
2023-01-25 01:47:23 +0600

    addd

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,12 +1,27 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import jwt from 'jsonwebtoken'
 
+const KEY = 'asdsadasdsadasdasdas'
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
-    console.log(req.body)
+
     // res.setHeader('Set-Cookie', 'areyouprogrammer=true;')
 
     // res.statusCode = 200
 
     // res.end('Hello! How are you')
-    res.json({ num: Math.floor(Math.random() * 10) })
+
+    if (!req.body) {
+        res.statusCode = 404
+        res.end('error')
+        return
+    }
+
+    const { username, password } = req.body
+    res.json({
+        token: jwt.sign({
+            username,
+            admin: username === 'admin' && password === 'admin'
+        }, KEY)
+    })
 }
